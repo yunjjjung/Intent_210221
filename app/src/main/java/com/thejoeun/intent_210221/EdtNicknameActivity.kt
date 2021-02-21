f0d74f1800e2016f62d8491b920d2bcecc61fbe8
최윤정 <jjjung1003@naver.com>
2021-02-21 14:13:17 +0900

    닉네임 변경 화면 추가

---
+++ app/src/main/java/com/thejoeun/intent_210221/EdtNicknameActivity.kt
@@ -0,0 +1,11 @@
+package com.thejoeun.intent_210221
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EdtNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edt_nickname)
+    }
+}
