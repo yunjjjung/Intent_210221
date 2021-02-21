83d67323590e4a37cee5e4b7021e731d64d9e53f
최윤정 <jjjung1003@naver.com>
2021-02-21 13:58:03 +0900

    전달받은 데이터를 활용 예제

--- app/src/main/java/com/thejoeun/intent_210221/SecondActivity.kt
+++ app/src/main/java/com/thejoeun/intent_210221/SecondActivity.kt
@@ -2,10 +2,19 @@ package com.thejoeun.intent_210221
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+//        목적지에 도착한 상황 => 첨부한 데이터를 받아주자. (어떤 형태의 자료인가?)
+
+        val receiveMessage = intent.getStringExtra("message")
+
+//        수신문구 표현 텍스트 뷰의 문구로 설정 => 상황마다 코드가 달라짐
+        receiveMessageTxt.text = receiveMessage
+
     }
 }
