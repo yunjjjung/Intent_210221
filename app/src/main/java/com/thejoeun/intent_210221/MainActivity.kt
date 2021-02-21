69d894b019476b86ef9c4470233a887642b3273e
최윤정 <jjjung1003@naver.com>
2021-02-21 12:46:18 +0900

    Intent 기초 활용

--- app/src/main/java/com/thejoeun/intent_210221/MainActivity.kt
+++ app/src/main/java/com/thejoeun/intent_210221/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.thejoeun.intent_210221
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        moveToFirstActivityBtn.setOnClickListener { 
+            
+//            firstActivity로 넘어가기
+
+            val myIntent = Intent(this, FirstActivity::class.java)
+            startActivity(myIntent) //비행기 발권할 때 myIntent를 가져가겟다
+
+        }
     }
 }
