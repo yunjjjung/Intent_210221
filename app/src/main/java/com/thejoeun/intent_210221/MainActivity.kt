9a5ead83368bad46935aee75dfcb621c9d62f7c6
최윤정 <jjjung1003@naver.com>
2021-02-21 13:51:20 +0900

    화면 이동시 데이터 첨부 => 받아서 사용은 안하고 있음

--- app/src/main/java/com/thejoeun/intent_210221/MainActivity.kt
+++ app/src/main/java/com/thejoeun/intent_210221/MainActivity.kt
@@ -24,6 +24,16 @@ class MainActivity : AppCompatActivity() {
             val inputContent = contentEdt.text.toString()
 
 //            두번째 화면 (SecondActivity) 으로 => inputContent의 값을 들고 => 이동
+            val myIntent = Intent(this, SecondActivity::class.java)
+
+//            인텐트의 부가정보로 들고갈 데이터들을 붙여주자
+            myIntent.putExtra("message", inputContent)
+
+//            필요시 여러개 데이터를 첨부 가능 (단순 예시)
+            myIntent.putExtra("출생년도", 1988)
+
+            startActivity(myIntent)
+
         }
     }
 }
