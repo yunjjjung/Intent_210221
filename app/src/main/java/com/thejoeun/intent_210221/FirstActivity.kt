9ff9a37f87c5dd643e5942b1b9ac65243075572c
최윤정 <jjjung1003@naver.com>
2021-02-21 13:35:16 +0900

    메인 화면 복귀 버튼 > finish() 실행

--- app/src/main/java/com/thejoeun/intent_210221/FirstActivity.kt
+++ app/src/main/java/com/thejoeun/intent_210221/FirstActivity.kt
@@ -2,10 +2,18 @@ package com.thejoeun.intent_210221
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first.*
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        backToMainBtn.setOnClickListener {
+
+//            이전 화면으로 돌아가기 => 현재 화면 종료
+            finish()
+
+        }
     }
 }
